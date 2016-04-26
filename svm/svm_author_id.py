2289185ab18519a26640848d000b3373e4ff9fd8
Guilherme Strachan <guilhermestrachan@gmail.com>
2016-04-25 23:34:16 -0300

    Add timer for training and prediction

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,9 +28,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 
 clf = SVC(kernel = 'linear')
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0,3), "s"
 
+t0 = time()
 pred = clf.predict(features_test)
+print "prediction time: ", round(time()-t0,3), "s"
 
 print clf.score(features_test, labels_test)
 
