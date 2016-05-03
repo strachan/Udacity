6cd7d0602924b7a15ae858100f444aaf32ea5653
Guilherme Strachan <guilhermestrachan@gmail.com>
2016-05-03 07:40:48 -0300

    Explore data for machine learning

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,57 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print 'number of people: ' + str(len(enron_data))
+
+print 'number of features: ' + str(len(enron_data.values()[0]))
+
+print 'number of pois: ' + str(len({k: v for k, v in enron_data.iteritems() if v['poi'] == 1})) 
+
+print 'James Prentices stocks value: ' + str(enron_data["PRENTICE JAMES"]["total_stock_value"])
+
+print 'Wesley Colwell emails to POI: ' + str(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
+
+print 'Jeffrey Skilling stocks value: ' + str(enron_data["SKILLING JEFFREY K"])
+
+print 'Andrew Fastow total payments: ' + str(enron_data["FASTOW ANDREW S"]["total_payments"])
+
+print 'Jeffrey Skilling total payments: ' + str(enron_data["SKILLING JEFFREY K"]["total_payments"])
+
+print 'Kenneth Lay total payments: ' + str(enron_data["LAY KENNETH L"]["total_payments"])
+
+
+salary = 0
+email = 0
+percentageTotalPaymentsFilled = 0
+percentagePoiTotalPaymentsFilled = 0
+numberOfPoi = 0
+
+for k, v in enron_data.iteritems():
+    if (enron_data[k]['salary'] == 'NaN'):
+        salary += 1
+    if (enron_data[k]['email_address'] == 'NaN'):
+        email += 1
+    if (enron_data[k]['total_payments'] == 'NaN'):
+        percentageTotalPaymentsFilled += 1
+    if (enron_data[k]['total_payments'] == 'NaN' and enron_data[k]['poi'] == 1):
+        percentagePoiTotalPaymentsFilled += 1
+    if (enron_data[k]['poi'] == 1):
+        numberOfPoi += 1
+
+print 'total people in dataset: ' + str(len(enron_data))
+print 'total people with NaN for payment: ' + str(percentageTotalPaymentsFilled)
+print 'number of poi: ' + str(numberOfPoi)
+
+print 'total with salary defined: ' + str(len(enron_data) - salary)
+print 'total with email defined: ' + str(len(enron_data) - email)    
+        
+percentageTotalPaymentsFilled = percentageTotalPaymentsFilled * 100 / float(len(enron_data))
+print 'percentage with payments filled: ' + str(percentageTotalPaymentsFilled)
+
+percentagePoiTotalPaymentsFilled = percentagePoiTotalPaymentsFilled * 100 / float(len(enron_data))
+print 'percentage poi with payments filled: ' + str(percentagePoiTotalPaymentsFilled)
+
+
+
+
 
